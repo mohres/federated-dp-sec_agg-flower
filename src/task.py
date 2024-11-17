91f35103d85780447973c0e9c27f411c4907607d
Mohammad Fares <mohres20@gmail.com>
2024-11-17 18:19:40 +0300

    Remove old net

--- src/task.py
+++ src/task.py
@@ -17,26 +17,6 @@ fds = {}
 configs = toml.load("pyproject.toml")
 
 
-class Net(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch_size = x.size(0)
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(batch_size, -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
-
-
 class CustomResNet(nn.Module):
     def __init__(self, in_channels=0, num_classes=0):
         super(CustomResNet, self).__init__()
